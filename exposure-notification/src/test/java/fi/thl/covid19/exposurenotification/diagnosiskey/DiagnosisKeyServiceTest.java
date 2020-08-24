bf2c553dbce5a45ea57f22da3d4fbcb47d904aa1
Jyrki Jakobsson <jyrki.jakobsson@solita.fi>
2020-08-24 17:01:42 +0300

    Fix diagnosis key filtering & clean up interval calculation code

--- exposure-notification/src/test/java/fi/thl/covid19/exposurenotification/diagnosiskey/DiagnosisKeyServiceTest.java
+++ exposure-notification/src/test/java/fi/thl/covid19/exposurenotification/diagnosiskey/DiagnosisKeyServiceTest.java
@@ -2,7 +2,6 @@ package fi.thl.covid19.exposurenotification.diagnosiskey;
 
 import fi.thl.covid19.exposurenotification.diagnosiskey.v1.TemporaryExposureKey;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +15,7 @@ import java.util.List;
 
 import static fi.thl.covid19.exposurenotification.diagnosiskey.IntervalNumber.to10MinInterval;
 import static java.time.ZoneOffset.UTC;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ActiveProfiles({"test", "nodb"})
@@ -36,6 +36,7 @@ public class DiagnosisKeyServiceTest {
     @Test
     public void filterAcceptsKeyInsideRange() {
         List<TemporaryExposureKey> keys = List.of(
+                generateAt(LocalDate.parse("2020-07-02")),
                 generateAt(LocalDate.parse("2020-07-03")),
                 generateAt(LocalDate.parse("2020-07-04")),
                 generateAt(LocalDate.parse("2020-07-05")),
@@ -49,25 +50,27 @@ public class DiagnosisKeyServiceTest {
                 generateAt(LocalDate.parse("2020-07-13")),
                 generateAt(LocalDate.parse("2020-07-14")),
                 generateAt(LocalDate.parse("2020-07-15")));
-        Assertions.assertEquals(keys, filter(keys, "2020-07-16T14:00:00Z"));
+        assertEquals(keys, filter(keys, "2020-07-16T14:00:00Z"));
+    }
+
+    @Test
+    public void filterAcceptsTodaysKeys() {
+        List<TemporaryExposureKey> keys = List.of(
+                generateAt(LocalDate.parse("2020-07-16"), 14*6),
+                generateAt(LocalDate.parse("2020-07-16"), 144));
+        assertEquals(keys, filter(keys, "2020-07-16T14:00:00Z"));
     }
 
     @Test
     public void filterRejectsOver14daysOldKey() {
-        List<TemporaryExposureKey> keys = List.of(generateAt(LocalDate.parse("2020-07-02")));
-        Assertions.assertEquals(List.of(), filter(keys, "2020-07-16T14:00:00Z"));
-    }
-
-    @Test
-    public void filterRejectsKeyOverlappingEnd() {
-        List<TemporaryExposureKey> keys = List.of(generateAt(LocalDate.parse("2020-07-16")));
-        Assertions.assertEquals(List.of(), filter(keys, "2020-07-16T14:00:00Z"));
+        List<TemporaryExposureKey> keys = List.of(generateAt(LocalDate.parse("2020-07-01")));
+        assertEquals(List.of(), filter(keys, "2020-07-16T14:00:00Z"));
     }
 
     @Test
     public void filterRejectsKeyInFuture() {
         List<TemporaryExposureKey> keys = List.of(generateAt(LocalDate.parse("2020-07-17")));
-        Assertions.assertEquals(List.of(), filter(keys, "2020-07-16T14:00:00Z"));
+        assertEquals(List.of(), filter(keys, "2020-07-16T14:00:00Z"));
     }
 
     private List<TemporaryExposureKey> filter(List<TemporaryExposureKey> keys, String now) {
@@ -75,7 +78,11 @@ public class DiagnosisKeyServiceTest {
     }
 
     private TemporaryExposureKey generateAt(LocalDate keyDate) {
+        return generateAt(keyDate, 144);
+    }
+
+    private TemporaryExposureKey generateAt(LocalDate keyDate, int rollingPeriod) {
         int interval = to10MinInterval(keyDate.atStartOfDay(UTC).toInstant());
-        return new TemporaryExposureKey("c9Uau9icuBlvDvtokvlNaA==", 1, interval, 144);
+        return new TemporaryExposureKey("c9Uau9icuBlvDvtokvlNaA==", 1, interval, rollingPeriod);
     }
 }
