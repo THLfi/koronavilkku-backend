0ba3148ef872f08e43836cc7e81f504e2e70f13c
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-09-08 15:18:53 +0300

    Tests for statistics

--- publish-token/src/test/java/fi/thl/covid19/publishtoken/PublishTokenDaoIT.java
+++ publish-token/src/test/java/fi/thl/covid19/publishtoken/PublishTokenDaoIT.java
@@ -6,29 +6,37 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Collections;
 
 import static fi.thl.covid19.publishtoken.Validation.SERVICE_NAME_MAX_LENGTH;
 import static fi.thl.covid19.publishtoken.Validation.USER_NAME_MAX_LENGTH;
 import static java.time.temporal.ChronoUnit.HOURS;
 import static net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils.repeat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles({"dev"})
 @AutoConfigureMockMvc
 public class PublishTokenDaoIT {
 
+    private static final String TOKEN_STAT_TABLE_NAME = "stats_tokens_created";
+    private static final String SMS_STAT_TABLE_NAME = "stats_sms_created";
+
     @Autowired
     private PublishTokenDao dao;
+
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @BeforeEach
     public void setUp() {
         dao.deleteTokensExpiredBefore(Instant.now().plus(48, HOURS));
+        deleteStatsRows();
     }
 
     @Test
@@ -36,6 +44,7 @@ public class PublishTokenDaoIT {
         PublishToken token = new PublishToken("testtoken", Instant.now(), Instant.now().plus(1, HOURS));
         assertTrue(dao.storeToken(token, LocalDate.now(), "testservice", "testuser"));
         assertFalse(dao.storeToken(token, LocalDate.now(), "testservice", "testuser"));
+        assertStatRowAdded(TOKEN_STAT_TABLE_NAME);
     }
 
     @Test
@@ -48,5 +57,22 @@ public class PublishTokenDaoIT {
     public void minimumLengthFieldsAreWrittenOk() {
         PublishToken token = new PublishToken("123456789012", Instant.now(), Instant.now().plus(1, HOURS));
         dao.storeToken(token, LocalDate.now(), "a", "a");
+    }
+
+    @Test
+    public void testSmsStatRowsAddedOk() {
+        dao.addStatsRow(Instant.now(), SMS_STAT_TABLE_NAME);
+        assertStatRowAdded(SMS_STAT_TABLE_NAME);
+    }
+
+    private void assertStatRowAdded(String tableName) {
+        String sql = "select count(*) from pt." + tableName;
+        Integer rows = jdbcTemplate.queryForObject(sql, Collections.emptyMap(), Integer.class);
+        assertEquals(1, rows);
+    }
+
+    private void deleteStatsRows() {
+        jdbcTemplate.update("delete from pt." + TOKEN_STAT_TABLE_NAME, Collections.emptyMap());
+        jdbcTemplate.update("delete from pt." + SMS_STAT_TABLE_NAME, Collections.emptyMap());
     }
 }
