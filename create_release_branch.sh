6d14d9a6e35760c32d2566440d77790077b1a942
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-09-09 13:26:56 +0300

    Initial implementation of release automatization

---
+++ create_release_branch.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -eu
+
+#
+# This script creates release branch and rolls version in trunk.
+#
+
+PROGRAM_NAME=$0
+
+usage() {
+    printf "usage: %s --version=[version]\n""$PROGRAM_NAME"
+    printf "version  Next version which will be updated to poms in trunk after release branch is created\n"
+    exit 1
+}
+
+check_parameters() {
+  if [ -z ${NEW_VERSION+x} ]; then usage; fi
+}
+
+for i in "$@"
+do
+case $i in
+    --version=*)
+    NEW_VERSION="${i#*=}"
+    shift
+    ;;
+    *)
+    ;;
+esac
+done
+
+check_parameters
+
+git fetch --all --tags --prune
+git checkout trunk
+git pull --rebase
+
+CURRENT_VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" pom.xml)
+
+git checkout -b release/$CURRENT_VERSION
+
+while true; do
+    read -rp "Do you want to push new release branch to remote?" yn
+    case $yn in
+        [Yy]* ) git push --set-upstream origin "release/$CURRENT_VERSION"; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+git checkout trunk
+
+find . -name "pom.xml" -exec sed -i "s|<version>$CURRENT_VERSION</version>|<version>$NEW_VERSION</version>|" {} \;
+
+git commit -a -m "Rolled version for new development version"
+
+echo "Do you want to push new version to remote?"
+while true; do
+    read -p "Do you want to push new version to remote?" yn
+    case $yn in
+        [Yy]* ) git push; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
