481c26622e33539d403a6997a0b9d8f66dda6c6e
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-11-03 12:00:15 +0200

    not null for new configuration columns

--- exposure-notification/src/main/resources/db/migration/V02.03__alter_configuration_table.sql
+++ exposure-notification/src/main/resources/db/migration/V02.03__alter_configuration_table.sql
@@ -1,2 +1,4 @@
-alter table en.exposure_configuration add duration_at_attenuation_weights decimal(3,2) array[3];
-alter table en.exposure_configuration add exposure_risk_duration int;
+alter table en.exposure_configuration add duration_at_attenuation_weights decimal(3,2) array[3] not null default '{ 1.0, 0.5, 0.0 }';
+alter table en.exposure_configuration alter column duration_at_attenuation_weights drop default;
+alter table en.exposure_configuration add exposure_risk_duration int not null default 15;
+alter table en.exposure_configuration alter column exposure_risk_duration drop default;
