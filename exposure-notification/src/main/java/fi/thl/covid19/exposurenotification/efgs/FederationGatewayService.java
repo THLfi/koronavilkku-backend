43aa310fb219b6b30ce907e703914fd58171ce05
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-11-04 16:18:24 +0200

    Split into batches

--- exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/FederationGatewayService.java
+++ exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/FederationGatewayService.java
@@ -15,13 +15,17 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static fi.thl.covid19.exposurenotification.diagnosiskey.IntervalNumber.utcDateOf10MinInterval;
 import static fi.thl.covid19.exposurenotification.diagnosiskey.TransmissionRiskBuckets.getRiskBucket;
 
 @Service
 public class FederationGatewayService {
+
+    private static final int BATCH_MAX_SIZE = 5000;
 
     private final FederationGatewayClient client;
     private final BatchFileService batchFileService;
@@ -40,17 +44,28 @@ public class FederationGatewayService {
     public void doOutbound() {
         long operationId = dd.startOutboundOperation();
         try {
-            List<TemporaryExposureKey> localKeys = dd.fetchAvailableKeysForEfgs(operationId);
-            //TODO: maximum batch size is 5000 keys in efgs, please split
-            ResponseEntity<UploadResponseEntity> res = handleOutbound(transform(localKeys), operationId);
+            List<List<TemporaryExposureKey>> localKeys = partitionIntoBatches(dd.fetchAvailableKeysForEfgs(operationId));
 
-            // 207 means partial success, due server implementation details we'll need to remove erroneous and re-send
-            if (res.getStatusCodeValue() == 207) {
-                handlePartialOutbound(res.getBody(), localKeys, operationId);
+            AtomicInteger total201Count = new AtomicInteger();
+            AtomicInteger total409Count = new AtomicInteger();
+            AtomicInteger total500Count = new AtomicInteger();
 
-            } else {
-                dd.finishOperation(operationId, localKeys.size());
-            }
+            localKeys.forEach(
+                    batch -> {
+                        ResponseEntity<UploadResponseEntity> res = handleOutbound(transform(batch), operationId);
+                        // 207 means partial success, due server implementation details we'll need to remove erroneous and re-send
+                        if (res.getStatusCodeValue() == 207) {
+                            Map<Integer, Integer> responseCounts = handlePartialOutbound(res.getBody(), batch, operationId);
+                            total201Count.addAndGet(responseCounts.get(201));
+                            total409Count.addAndGet(responseCounts.get(409));
+                            total500Count.addAndGet(responseCounts.get(409));
+                        } else {
+                            total201Count.addAndGet(batch.size());
+                        }
+                    }
+            );
+
+            dd.finishOperation(operationId, localKeys.size(), total201Count.get(), total409Count.get(), total500Count.get());
         } catch (Exception e) {
             // TODO: check what we really want to catch in here
             dd.markErrorOperation(operationId);
@@ -102,12 +117,18 @@ public class FederationGatewayService {
         return getRiskBucket(LocalDate.from(keyDate).plusDays(key.getDaysSinceOnsetOfSymptoms()), keyDate);
     }
 
+    private List<List<TemporaryExposureKey>> partitionIntoBatches(List<TemporaryExposureKey> objs) {
+        return new ArrayList<>(IntStream.range(0, objs.size()).boxed().collect(
+                Collectors.groupingBy(e -> e / BATCH_MAX_SIZE, Collectors.mapping(objs::get, Collectors.toList())
+                )).values());
+    }
+
     private ResponseEntity<UploadResponseEntity> handleOutbound(EfgsProto.DiagnosisKeyBatch batch, long operationId) {
         byte[] batchData = serialize(batch);
         return client.upload(getDateString(Instant.now()) + "-" + operationId, calculateBatchSignature(batchData), batchData);
     }
 
-    private void handlePartialOutbound(UploadResponseEntity body, List<TemporaryExposureKey> localKeys, long operationId) {
+    private Map<Integer, Integer> handlePartialOutbound(UploadResponseEntity body, List<TemporaryExposureKey> localKeys, long operationId) {
         List<Integer> successKeysIdx = Objects.requireNonNull(body).get(201);
         List<Integer> keysIdx409 = Objects.requireNonNull(body).get(409);
         List<Integer> keysIdx500 = Objects.requireNonNull(body).get(500);
@@ -118,7 +139,7 @@ public class FederationGatewayService {
         if (resendRes.getStatusCodeValue() == 207)
             throw new EfgsOperationException("Upload to efgs still failing after resend.");
 
-        dd.finishOperation(operationId, localKeys.size(), successKeysIdx.size(), keysIdx409.size(), keysIdx500.size());
+        return Map.of(201,successKeysIdx.size(), 409, keysIdx409.size(), 500, keysIdx500.size());
     }
 
     private String calculateBatchSignature(byte[] data) {
