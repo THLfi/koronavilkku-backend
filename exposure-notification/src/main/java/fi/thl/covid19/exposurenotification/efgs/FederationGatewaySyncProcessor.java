d86f9ff0c3be0ab2511f949f6ce0be3ed53e22b6
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-11-15 11:15:02 +0200

    Revert "changed to cron scheduling to make run schedule more predictable"
    This reverts commit dbe4ab1866b460cbee735446b60044cae0ce7d0a.

--- exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/FederationGatewaySyncProcessor.java
+++ exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/FederationGatewaySyncProcessor.java
@@ -23,21 +23,24 @@ public class FederationGatewaySyncProcessor {
         this.federationGatewayService = federationGatewayService;
     }
 
-    @Scheduled(cron = "${covid19.federation-gateway.upload-sched}")
+    @Scheduled(initialDelayString = "${covid19.federation-gateway.upload-interval}",
+            fixedRateString = "${covid19.federation-gateway.upload-interval}")
     private void runExportToEfgs() {
         LOG.info("Starting scheduled export to efgs.");
         Optional<Set<Long>> operationIds = federationGatewayService.startOutbound(false);
         LOG.info("Scheduled export to efgs finished. {}", keyValue("operationId", operationIds.orElse(Set.of())));
     }
 
-    @Scheduled(cron = "${covid19.federation-gateway.download-sched}")
+    @Scheduled(initialDelayString = "${covid19.federation-gateway.download-interval}",
+            fixedRateString = "${covid19.federation-gateway.download-interval}")
     private void runImportFromEfgs() {
         LOG.info("Starting scheduled import from efgs.");
         federationGatewayService.startInbound(LocalDate.now(ZoneOffset.UTC), Optional.empty());
         LOG.info("Scheduled import from efgs finished.");
     }
 
-    @Scheduled(cron = "${covid19.federation-gateway.error-handling-sched}")
+    @Scheduled(initialDelayString = "${covid19.federation-gateway.error-handling-interval}",
+            fixedRateString = "${covid19.federation-gateway.error-handling-interval}")
     private void runErrorHandling() {
         LOG.info("Starting scheduled efgs error handling.");
         federationGatewayService.startOutbound(true);
