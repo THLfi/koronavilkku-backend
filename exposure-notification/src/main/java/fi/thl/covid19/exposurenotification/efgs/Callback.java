ef513dc36c61f1bf157df5af3b20cbcc127a85b3
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-11-20 16:24:21 +0200

    added requirenonnull

--- exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/Callback.java
+++ exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/efgs/Callback.java
@@ -1,12 +1,14 @@
 package fi.thl.covid19.exposurenotification.efgs;
 
+import static java.util.Objects.requireNonNull;
+
 public class Callback {
 
     public final String callbackId;
     public final String url;
 
     public Callback(String callbackId, String url) {
-        this.callbackId = callbackId;
-        this.url = url;
+        this.callbackId = requireNonNull(callbackId);
+        this.url = requireNonNull(url);
     }
 }
