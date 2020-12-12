a7ae80e993c5593b058e50a3a93f6b96726b6639
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2020-12-12 09:45:05 +0200

    made schema check conditional on property

--- publish-token/src/main/java/fi/thl/covid19/publishtoken/error/DbSchemaCheckHealthIndicator.java
+++ publish-token/src/main/java/fi/thl/covid19/publishtoken/error/DbSchemaCheckHealthIndicator.java
@@ -2,6 +2,7 @@ package fi.thl.covid19.publishtoken.error;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,9 @@ import java.util.Map;
 import static java.util.Objects.requireNonNull;
 
 @Repository
+@ConditionalOnProperty(
+        prefix = "covid19.db-schema-check", value = "enabled"
+)
 public class DbSchemaCheckHealthIndicator implements HealthIndicator {
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
