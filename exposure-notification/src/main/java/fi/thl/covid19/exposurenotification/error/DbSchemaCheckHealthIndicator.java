a8ca37b94dfbceb1da72733057b0cf20433d424d
Alpertti Tirronen <62981719+solita-alperttiti@users.noreply.github.com>
2020-12-14 07:46:54 +0200

    Sanitizing db health check (#47)
    * moved db schema check for more general package and added it into pt also + minor dev log conf tuning
    
    * made schema check conditional on property

---
+++ exposure-notification/src/main/java/fi/thl/covid19/exposurenotification/error/DbSchemaCheckHealthIndicator.java
@@ -0,0 +1,44 @@
+package fi.thl.covid19.exposurenotification.error;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
+
+@Repository
+@ConditionalOnProperty(
+        prefix = "covid19.db-schema-check", value = "enabled"
+)
+public class DbSchemaCheckHealthIndicator implements HealthIndicator {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public DbSchemaCheckHealthIndicator(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = requireNonNull(jdbcTemplate);
+    }
+
+    @Override
+    public Health health() {
+        if (checkEnSchemaExists()) {
+            return Health.up().build();
+        } else {
+            return Health.down().withDetail("Db schema en does not exists or unspecified db query error", "FAILED").build();
+        }
+    }
+
+    private boolean checkEnSchemaExists() {
+        String sql = "select count(*) from information_schema.schemata where schema_name = :schema_name";
+        try {
+            return jdbcTemplate.query(sql, Map.of("schema_name", "en"),
+                    (rs, i) -> rs.getInt(1)).stream().findFirst().orElseThrow(() -> new IllegalStateException("Count returned nothing.")) > 0;
+        } catch (DataAccessException | IllegalStateException e) {
+            return false;
+        }
+    }
+}
