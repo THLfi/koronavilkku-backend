b2f0ddf46a1c674cbb6cc2c156b537463b5e123f
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2021-01-11 18:50:43 +0200

    Revert "reverted hstore migrations for now"
    This reverts commit 7e44098c3c1d8fba643324d0e30aa499d0c0c7a7.

---
+++ exposure-notification/src/main/resources/db/migration/V03.05__exposure_configuration_v2.sql
@@ -0,0 +1,12 @@
+create extension if not exists hstore schema public cascade;
+create table en.exposure_configuration_v2 (
+  version int primary key generated always as identity,
+  report_type_weights hstore not null,
+  infectiousness_weights hstore not null,
+  attenuation_bucket_threshold_db numeric array[3] not null,
+  attenuation_bucket_weights numeric array[4] not null,
+  days_since_exposure_threshold int not null,
+  minimum_window_score numeric not null,
+  days_since_onset_to_infectiousness hstore not null,
+  available_countries varchar(2)[] not null
+);
