52124502223ae2b83d7d0eddec6d65be11bddc30
Alpertti Tirronen <alpertti.tirronen@solita.fi>
2021-02-26 16:47:08 +0200

    add default v2 interval for existing keys

--- exposure-notification/src/main/resources/db/migration/V04.02__diagnosis_key_v2.sql
+++ exposure-notification/src/main/resources/db/migration/V04.02__diagnosis_key_v2.sql
@@ -1,1 +1,2 @@
-alter table en.diagnosis_key add submission_interval_v2 int not null;
+alter table en.diagnosis_key add submission_interval_v2 int not null default 0;
+alter table en.diagnosis_key alter column submission_interval_v2 drop default;
